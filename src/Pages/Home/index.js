ea0136450037733e65dc201648807c8580b3a1c9
Muhammad Dicky Afriza <afrizadicky6@gmail.com>
2022-04-07 13:30:49 +0700

    home1

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,15 +1,33 @@
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import React from 'react';
+import {Header, Carousel} from '../../components';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.page}>
+    <Header title={'Beranda'} />
+    </SafeAreaView>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  productList: {
+    paddingHorizontal: 16,
+  },
+  separator: {
+    height: 16,
+  },
+  listProduct: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#2F2E41',
+    marginBottom: 16,
+  },
+});
